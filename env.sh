026d3adfdd8d88fbe51a9d7e0f647871fa12ba65
Takanari Ko <takou@zlab.co.jp>
2016-03-15 11:54:16 +0900

    fix

--- env.sh
+++ env.sh
@@ -1,8 +1,4 @@
 #!/bin/sh
-cat package.json \
- | sed -e "s|\$PUBLISH_REGISTRY|$PUBLISH_REGISTRY|g" \
- > /tmp/package.json && mv /tmp/package.json package.json
 cat .npmrc \
  | sed -e "s|\$REGISTRY|$REGISTRY|g" \
- | sed -e "s|\$API_KEY|$API_KEY|g" \
  > /tmp/.npmrc && mv /tmp/.npmrc .npmrc
